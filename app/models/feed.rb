2e837c38d7a88acee2d4ed85aa3b53f77ea5ebcc
kurenn <abraham.kuri@gmail.com>
2011-04-18 15:41:10 +1000

    just added some validations for the feed model and product model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,6 @@
 class Feed < ActiveRecord::Base
   attr_accessible :title, :author, :content
+  validates_presence_of :title, :on => :create, :message => "can't be blank"
+  validates_presence_of :author, :on => :create, :message => "can't be blank"
+  validates_presence_of :content, :on => :create, :message => "can't be blank"
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :comments, :as => :commentable, :dependent => :destroy
   attr_accessible :name, :description, :category_id
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_presence_of :description, :on => :create, :message => "can't be blank"
+  validates_presence_of :category_id, :on => :create, :message => "can't be blank"
 end

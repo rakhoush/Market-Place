4b40fa8aa5269cae76ff4ba5fea734618cec302d
kurenn <abraham.kuri@gmail.com>
2011-04-24 21:33:08 +1200

    the articles CRUD is ready and working

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418055245) do
+ActiveRecord::Schema.define(:version => 20110424092610) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"

8ee44f2710d4995b41cf6894b5c3a7eada5857e2
kurenn <abraham.kuri@gmail.com>
2011-05-06 14:53:33 +1000

    the comparison between products is done in the homepage

--- app/models/storeproduct.rb
+++ app/models/storeproduct.rb
@@ -3,6 +3,6 @@ class Storeproduct < ActiveRecord::Base
   belongs_to :product
   
    def self.find_by_store_and_product_id(store_id,product_id)
-        Storeproduct.find(:first, :conditions => "store_id = #{store_id} AND product_id = #{product_id}")
+        find(:first, :conditions => "store_id = #{store_id} AND product_id = #{product_id}")
    end
 end

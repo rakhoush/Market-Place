db7170b33674cf9f1fb0099368ae53ff89053a37
kurenn <abraham.kuri@gmail.com>
2011-05-13 23:54:54 +1000

    adding the gitignore file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+User.create!(:name => "Abraham", 
+             :email => "abraham.kuri@gmail.com", 
+             :password => "frxhgroi", 
+             :password_confirmation => "frxhgroi", 
+             :admin => "1")
+
+User.create!(:name => "Andrei", 
+            :email => "andrej.berglund@gmail.com", 
+            :password => "marketplace", 
+            :password_confirmation => "marketplace", 
+            :admin => "1")
